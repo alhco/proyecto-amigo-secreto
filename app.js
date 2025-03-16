23dcaadb17e4edb609c7d61bd4a6577d8508aa74
Aldo Hernandez <aldohdzco@gmail.com>
2025-03-16 05:38:17 -0500

    Modifica titulo del juego y declaro primeras variabless

--- app.js
+++ app.js
@@ -1,2 +1,15 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 // Prueba 
+// Declarando Array para almacenar nombres de amigos
+let amigos = [];
+// Uniendo HTML CON JS y probando cambiar titulo.
+document.querySelector('h1');
+let titulo = document.querySelector('h1');
+titulo.innerHTML = 'Juego del amigo secreto por Aldo';
+// Habilitar botones
+
+
+// Función para agregar amigos
+function NuevoAmigo(nombre){
+    ;
+}
